143232dc2e67f4a705f94689f9c0e7a5995f5de5
Nicholas Willhite <willhite.nicholas@gmail.com>
2017-09-01 11:01:23 -0700

    added script iiq_gather_info

---
+++ iiqtools/iiq_gather_info.py
@@ -0,0 +1,305 @@
+# -*- coding: UTF-8 -*-
+"""
+This module contains all the business logic for collecting logs and configuration
+information about InsightIQ for remote troubleshooting.
+"""
+import os
+import sys
+import time
+import json
+import glob
+import tarfile
+import argparse
+import traceback
+import subprocess
+from getpass import getuser
+from StringIO import StringIO
+
+try:
+    from iiq_data_export.api_connection import iiq_api
+    from insightiq.lib.api_connection.api import APIConnectionError
+except ImportError:
+    # InsightIQ is closed source, so it cannot be included as a dev/test dependency.
+    # We're stuck with assuming a dev/test environment, and mocking it away.
+    # This is the only way we can do unit testing, and yes, it's a bit hacky.
+    from mock import MagicMock
+    iiq_api = MagicMock()
+    APIConnectionError = RuntimeError
+
+from iiqtools.utils import cli_parsers
+from iiqtools.utils.logger import get_logger
+from iiqtools.utils.shell import run_cmd
+from iiqtools.exceptions import CliError
+
+
+def get_tarfile(output_dir, case_number, the_time=None):
+    """Centralizes logic for making tgz file for InsightIQ logs
+
+    :Returns: tarfile.TarFile
+
+    :param output_dir: **Required** The directory to save the tar file in
+    :type output_dir: String
+
+    :param case_number: **Required** The SR that the logs are for; used in file name.
+    :type case_number: String or Integer
+
+    :param the_time: An optional EPOC timestamp to use when naming the file.
+                     If not supplied, this function calls time.time().
+    :type the_time: EPOC time stamp
+    """
+    base_dir = os.path.abspath(output_dir)
+    if the_time is None:
+        the_time = int(time.time())
+    file_name = 'IIQLogs-sr%s-%s.tgz' % (case_number, the_time)
+    full_path = os.path.join(base_dir, file_name)
+    return tarfile.open(full_path, 'w:gz') # 2nd param makes it a tgz file
+
+
+def add_from_memory(the_tarfile, data_name, data):
+    """Simplify adding in-memory information to the tar file
+
+    :Returns: None
+
+    :param the_tarfile: The open tarfile object
+    :type the_tarfile: tarfile.open
+
+    :param data_name: The reference to the data; i.e. it's name when you uncompress the file
+    :type data_name: String
+
+    :param data: The contents of the in-memory information
+    :type data: String
+    """
+    info = tarfile.TarInfo(data_name)
+    info.size = len(data)
+    info.mtime = time.time()
+    info.mode = int('444', 8) # everyone can read in oct, not decimal
+    the_tarfile.addfile(info, StringIO(data))
+
+
+def check_path(cli_value):
+    """Validate that the supplied path is an actual file system path
+
+    :Returns: String
+
+    :param cli_value: The value supplied by the end user
+    :type cli_value: String
+    """
+    if not os.path.isdir(cli_value):
+        msg = 'Supplied value is not a directory, %s' % (cli_value)
+        raise argparse.ArgumentTypeError(msg)
+    else:
+        return cli_value
+
+
+def parse_cli(cli_args):
+    """Handles parsing the CLI, and gives us --help for (basically) free
+
+    :Returns: argparse.Namespace
+
+    :param cli_args: The arguments passed to the script
+    :type cli_args: List
+    """
+    parser = argparse.ArgumentParser(description='Generate a .tar file for debugging InsightIQ',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--output-dir', default='/home/administrator', type=check_path,
+        help='The directory to write the .tgz file to')
+    parser.add_argument('--case-number', required=True,
+        help='The Service Request number. Used in naming tar file.')
+
+    args = parser.parse_args(cli_args)
+    return args
+
+
+# IIQ API info
+def call_iiq_api(uri):
+    """Make an internal API call to the InsightIQ API
+
+    :Returns: JSON String
+
+    :param uri: The API end point to call
+    :type uri: string
+    """
+    template = {'category' : 'api',
+                'endpoint' : uri,
+                'error' : '',
+                'response' : None,
+                'traceback' : ''}
+    try:
+        resp = iiq_api.make_request(uri)
+    except APIConnectionError:
+        template['error'] = "unable to query InsightIQ API"
+    except Exception as doh:
+        template['error'] = "%s" % doh
+        template['traceback'] = traceback.format_exc()
+    else:
+        data = json.loads(resp.read())
+        template['response'] = data
+
+    # The indent makes the JSON more human friendly, which GS appreciates
+    return json.dumps(template, indent=4, sort_keys=True)
+
+
+def datastore_info():
+    """Obtain data about the datastore for InsightIQ
+
+    :Returns: JSON String
+    """
+    return call_iiq_api('/api/datastore/usage?current_dir=true')
+
+
+def clusters_info():
+    """Obtain a pile of data about all monitored clusters in InsightIQ
+
+    :Returns: JSON String
+    """
+    return call_iiq_api('/api/clusters')
+
+
+def ldap_info():
+    """Obtain the config for LDAP in InsightIQ
+
+    :Returns: JSON String
+    """
+    return call_iiq_api('/api/ldap/configs')
+
+
+def reports_info():
+    """Obtain info about any scheduled reports in InsightIQ
+
+    :Returns: JSON String
+    """
+    return call_iiq_api('/api/reports')
+
+
+# CLI command info
+def cli_cmd_info(command, parser):
+    """Standardizes the JSON format for any data collected via a CLI command
+
+    :Returns: JSON String
+
+    :param command: The CLI command to execute
+    :type command: String
+    """
+    try:
+        result = run_cmd(command)
+    except CliError as result:
+        pass
+    template = {'exitcode' : result.exit_code,
+                'stderr' : result.stderr,
+                'command' : result.command,
+                'stdout' : result.stdout,
+                'category' : 'cli',
+                'traceback' : ''}
+    if not (result.exit_code or result.stderr):
+        # I know, shitty heuristic to determine errors...
+        try:
+            parsed_format = parser(result.stdout)
+        except Exception:
+            template['traceback'] = traceback.format_exc()
+        else:
+            template.update(parsed_format)
+
+    return json.dumps(template, indent=4, sort_keys=True)
+
+
+def mount_info():
+    """Obtain data about mounted file systems on the host OS
+
+    :Returns: JSON String
+    """
+    return cli_cmd_info('df -P', cli_parsers.df_to_dict)
+
+
+def memory_info():
+    """Obtain data about RAM on the host OS
+
+    :Returns: JSON String
+    """
+    return cli_cmd_info('free -m', cli_parsers.memory_to_dict)
+
+
+def ifconfig_info():
+    """Obtain data about the network interfaces on the host OS
+
+    :Returns: JSON String
+    """
+    return cli_cmd_info('ifconfig', cli_parsers.ifconfig_to_dict)
+
+
+def iiq_version_info():
+    """Obtain info about the version of InsightIQ installed on the host OS
+
+    :Returns: JSON String
+    """
+    return cli_cmd_info('rpm -q isilon-insightiq', cli_parsers.iiq_version_to_dict)
+
+
+def main(the_cli_args):
+    """Entry point for running script"""
+    args = parse_cli(the_cli_args)
+    user = getuser()
+    if user != 'root':
+        warning = '\n****WARNING****\nNot all data is going to be collected because\n'
+        warning += 'some log files can only be read by root.\n'
+        warning += 'To collect all diagnostic data, you must run as root.\n'
+        warning += 'You are currently running as %s\n' % user
+        print(warning)
+        response = raw_input('Do you wish to continue? (yes/no): ')
+        if not response.lower().startswith('y'):
+            return 1
+
+    log = get_logger(log_path='/dev/null', stream_lvl=10, file_lvl=10)
+    try:
+        tar_file = get_tarfile(args.output_dir, args.case_number)
+    except (IOError, OSError) as doh:
+        log.error(doh)
+        return doh.errno
+
+    log.info('Collecting config information')
+    add_from_memory(tar_file, 'iiq_version.json', iiq_version_info())
+    add_from_memory(tar_file, 'ifconfig.json', ifconfig_info())
+    add_from_memory(tar_file, 'memory.json', memory_info())
+    add_from_memory(tar_file, 'df.json', mount_info())
+
+    add_from_memory(tar_file, 'ldap.json', ldap_info())
+    add_from_memory(tar_file, 'clusters.json', clusters_info())
+    add_from_memory(tar_file, 'datastore.json', datastore_info())
+
+    # add release info files
+    for releaseinfo in glob.glob('/etc/*release'):
+        tar_file.add(releaseinfo)
+
+    # add config files
+    for a_file in glob.glob('/etc/isilon/*'):
+        tar_file.add(a_file)
+    for rabbitconfig in glob.glob('/etc/rabbitmq/*.config'):
+        tar_file.add(rabbitconfig)
+
+    log.info('Collecting log files')
+    # add iiq log files
+    for iiq_log in glob.glob('/var/log/insightiq*'):
+        tar_file.add(iiq_log)
+    for iiq_log in glob.glob('/var/log/iiq*'):
+        tar_file.add(iiq_log)
+    for iiq_log in glob.glob('/var/log/insightiq_clusters/*/*'):
+        tar_file.add(iiq_log)
+
+    # add postgres logs
+    try:
+        tar_file.add('/var/cache/insightiq/pgsql/pgstartup.log')
+    except (IOError, OSError):
+        # It's a permissions error; already prompted user that not all files
+        # can be collected if not ran as root
+        pass
+    for pglog in glob.glob('/var/log/pg_log/*'):
+        tar_file.add(pglog)
+
+    # add rabbitmq logs
+    for rabbitlog in glob.glob('/var/log/rabbitmq/*'):
+        tar_file.add(rabbitlog)
+
+    tar_file.close()
+    log.info('Log gather complete')
+    log.info('Created log file %s', tar_file.name)
+    return 0
---
+++ tests/test_iiq_gather_info.py
@@ -0,0 +1,356 @@
+# -*- coding: UTF-8 -*-
+"""
+Unit tests for the iiqtools.iiq_gather_info logic
+"""
+import os
+import json
+import gzip
+import glob
+import unittest
+import argparse
+import __builtin__
+
+from mock import patch, MagicMock
+
+from iiqtools import iiq_gather_info
+
+
+class TestGetTarfile(unittest.TestCase):
+    """A suite of test cases for the iiq_gather_info.get_tarfile function"""
+
+    def setUp(self):
+        """Runs before every test case"""
+        self.output_dir = '/tmp'
+        self.case_number = 1
+        self.the_time = 1234
+
+    def tearDown(self):
+        """Runs after every test case"""
+        test_files = glob.glob('/tmp/IIQLogs*')
+        for the_file in test_files:
+            os.remove(the_file)
+
+    def test_get_tarfile_name(self):
+        """The `get_tarfile` function returns the expected file"""
+        the_tarfile = iiq_gather_info.get_tarfile(self.output_dir,
+                                                  self.case_number,
+                                                  the_time=self.the_time)
+        tarfile_name = the_tarfile.name
+        expected_name = '{}/IIQLogs-sr{}-{}.tgz'.format(self.output_dir,
+                                                        self.case_number,
+                                                        self.the_time)
+
+        self.assertEqual(tarfile_name, expected_name)
+
+    def test_get_tarfile_is_compressed(self):
+        """The `get_tarfile` function returns a gzipped file"""
+        the_tarfile = iiq_gather_info.get_tarfile(self.output_dir,
+                                                  self.case_number)
+
+        self.assertTrue(isinstance(the_tarfile.fileobj, gzip.GzipFile))
+
+
+
+class TestAddFromMemory(unittest.TestCase):
+    """A suite of tests for the iiq_gather_info.add_from_memory function"""
+
+    def setUp(self):
+        """Runs before every test case"""
+        self.fake_tarfile = MagicMock()
+
+    def test_add_from_memory_addfile(self):
+        """The add_from_memory function adds the data to the tar file"""
+
+        iiq_gather_info.add_from_memory(the_tarfile=self.fake_tarfile,
+                                        data_name='foo.json',
+                                        data='{"some" : "json string"}')
+        call_count = self.fake_tarfile.addfile.call_count
+        expected = 1
+
+        self.assertEqual(call_count, expected)
+
+    @patch.object(iiq_gather_info.tarfile, 'TarInfo')
+    def test_add_from_memory_mode(self, fake_tarinfo):
+        """The add_from_memory fuction sets the correct POSIX permissions"""
+        fake_info = MagicMock()
+        fake_tarinfo.return_value = fake_info
+        iiq_gather_info.add_from_memory(the_tarfile=self.fake_tarfile,
+                                        data_name='foo.json',
+                                        data='{"some" : "json string"}')
+        posix_oct = fake_info.mode
+        expected = 292 # that's 444 POSIX in decimal
+
+        self.assertEqual(posix_oct, expected)
+
+class TestPathCallback(unittest.TestCase):
+    """A suite of tests for the iiq_gather_info.check_path function"""
+
+    def setUp(self):
+        """Runs before every test case"""
+        self.junk_file = '/tmp/asdfoowersdfuixlwles'
+
+    def tearDown(self):
+        """Runs after every test case"""
+        try:
+            os.remove(self.junk_file)
+        except OSError as doh:
+            if doh.errno == 2:
+                # No such File, ignore
+                pass
+            else:
+                raise
+
+    def test_invalid_path(self):
+        """Supplying an invalid file system path raises argparse.ArgumentTypeError"""
+        self.assertRaises(argparse.ArgumentTypeError, iiq_gather_info.check_path, 'foo')
+
+    def test_supply_directory(self):
+        """Supplying an actual directory returns that value"""
+        supplied_value = '/tmp'
+        returned_value = iiq_gather_info.check_path(supplied_value)
+
+        self.assertEqual(supplied_value, returned_value)
+
+    def test_supply_file(self):
+        """Supplying a file (not a directory) raises argparse.ArgumentTypeError"""
+        f = open(self.junk_file, 'w')
+        f.close()
+        self.assertRaises(argparse.ArgumentTypeError, iiq_gather_info.check_path, self.junk_file)
+
+
+class TestParseCli(unittest.TestCase):
+    """A suite of test cases for the parse_cli function"""
+
+    def test_returns_namespace(self):
+        """Supplying all required args to iiq_gather_info.parse_cli returns a namespace object"""
+        sent_args = ['--output-dir', '/tmp', '--case-number', '2']
+        out_args = iiq_gather_info.parse_cli(sent_args)
+
+        self.assertTrue(isinstance(out_args, argparse.Namespace))
+
+    # Mocking away stderr to avoid spam in output while running tests
+    @patch.object(iiq_gather_info.argparse._sys, 'stderr')
+    def test_no_args(self, fake_stderr):
+        """Passing zero args to iiq_gather_info.parse_cli raises SystemExit"""
+        sent_args = []
+        self.assertRaises(SystemExit, iiq_gather_info.parse_cli, sent_args)
+
+    @patch.object(iiq_gather_info.argparse._sys, 'stderr')
+    def test_only_output_dir(self, fake_stderr):
+        """Supplying only --output-dir raises SystemExit"""
+        sent_args = ['--output-dir', '/tmp',]
+        self.assertRaises(SystemExit, iiq_gather_info.parse_cli, sent_args)
+
+    @patch.object(iiq_gather_info.argparse._sys, 'stderr')
+    def test_only_case_number(self, fake_stderr):
+        """Supplying only --case-number raises SystemExit"""
+        sent_args = ['--case-number', '1',]
+        self.assertRaises(SystemExit, iiq_gather_info.parse_cli, sent_args)
+
+
+class TestCallIiqApi(unittest.TestCase):
+    """A suite of test cases for the call_iiq_api function"""
+
+    def setUp(self):
+        """Runs before every test case"""
+        # InsightIQ is closed source, so in the lib we replace the iiq_api
+        # function (which we cannot import) with a mock object
+        iiq_gather_info.iiq_api = MagicMock()
+        self.fake_response = MagicMock()
+        self.fake_response.read.return_value = '{"foo":"bar"}'
+        iiq_gather_info.iiq_api.make_request.return_value = self.fake_response
+
+    def test_response_is_json(self):
+        """The response is seralized, valid JSON"""
+        response = iiq_gather_info.call_iiq_api(uri='/foo')
+        deseralized = json.loads(response)
+
+        self.assertTrue(isinstance(response, str))
+        self.assertTrue(isinstance(deseralized, dict))
+        self.assertEqual(deseralized['response'], {'foo' : 'bar'})
+
+    def test_api_error(self):
+        """The response object notes if the IIQ API failed"""
+        iiq_gather_info.iiq_api.make_request.side_effect = [RuntimeError('testing')]
+
+        response = iiq_gather_info.call_iiq_api(uri='/foo')
+        data = json.loads(response)
+        err_msg = 'unable to query InsightIQ API'
+
+        self.assertEqual(err_msg, data['error'])
+
+    def test_notes_tracebacks(self):
+        """Unexpected exceptions have the assocated traceback logged in the response"""
+        # So we can A) fix it, and B) write a unit test for it! :D
+        iiq_gather_info.iiq_api.make_request.side_effect = [Exception('testing')]
+
+        response = iiq_gather_info.call_iiq_api(uri='/foo')
+        data = json.loads(response)
+        err_msg = 'testing'
+
+        self.assertEqual(err_msg, data['error'])
+        self.assertTrue(data['traceback'].startswith('Traceback'))
+
+
+class TestIiqApiHelpers(unittest.TestCase):
+    """A suite of test cases for the helper functions that pull
+       data from the InsightIQ API
+    """
+
+    def setUp(self):
+        """Runs before every test case"""
+        # InsightIQ is closed source, so in the lib we replace the iiq_api
+        # function (which we cannot import) with a mock object
+        iiq_gather_info.iiq_api = MagicMock()
+        self.fake_response = MagicMock()
+        self.fake_response.read.return_value = '{"foo":"bar"}'
+        iiq_gather_info.iiq_api.make_request.return_value = self.fake_response
+
+    def test_datastore_info(self):
+        """Function `datastore_info` calls the correct API endpoint"""
+        data = iiq_gather_info.datastore_info()
+        the_json = json.loads(data)
+        expected_endpoint = '/api/datastore/usage?current_dir=true'
+
+        self.assertEqual(the_json['endpoint'], expected_endpoint)
+
+    def test_clusters_info(self):
+        """Function `clusters_info` calls the correct API endpoint"""
+        data = iiq_gather_info.clusters_info()
+        the_json = json.loads(data)
+        expected_endpoint = '/api/clusters'
+
+        self.assertEqual(the_json['endpoint'], expected_endpoint)
+
+    def test_ldap_info(self):
+        """Function `ldap_info` calls the correct API endpoint"""
+        data = iiq_gather_info.ldap_info()
+        the_json = json.loads(data)
+        expected_endpoint = '/api/ldap/configs'
+
+        self.assertEqual(the_json['endpoint'], expected_endpoint)
+
+    def test_reports_info(self):
+        """Function `reports_info` calls the correct API endpoint"""
+        data = iiq_gather_info.reports_info()
+        the_json = json.loads(data)
+        expected_endpoint = '/api/reports'
+
+        self.assertEqual(the_json['endpoint'], expected_endpoint)
+
+
+class TestCliCmdInfo(unittest.TestCase):
+    """A suite of tests for the cli_cmd_info function"""
+
+    def test_might_fail(self):
+        """Verify that cli_cmd_info works without mocks"""
+        # the `free -m` command is most likely on every system, thus most likely to work
+        data = iiq_gather_info.cli_cmd_info('free -m', iiq_gather_info.cli_parsers.memory_to_dict)
+        the_json = json.loads(data)
+        expected_cmd = 'free -m'
+
+        self.assertEqual(the_json['command'], expected_cmd)
+
+    def test_bad_command(self):
+        """Supplying a bad CLI command to `cli_cmd_info` returns valid JSON still"""
+        data = iiq_gather_info.cli_cmd_info('sdfwehsxiodaweh', iiq_gather_info.cli_parsers.memory_to_dict)
+        the_json = json.loads(data)
+
+        self.assertTrue(the_json['exitcode'] != 0)
+
+    def test_parser_failure(self):
+        """A good command, and parser failure still generates valid JSON"""
+        data = iiq_gather_info.cli_cmd_info('date', iiq_gather_info.cli_parsers.memory_to_dict)
+        the_json = json.loads(data)
+
+        self.assertTrue(the_json['traceback'].startswith('Traceback'))
+
+
+class TestCliCmdInfoHelpers(unittest.TestCase):
+    """A suite of tests for the helpers for obtaining CLI command output"""
+
+    @patch.object(iiq_gather_info, 'cli_cmd_info')
+    def test_mount_info(self, fake_cli_cmd_info):
+        """iiq_gather_info.mount_info calls the expected command"""
+        iiq_gather_info.mount_info()
+        args, _ = fake_cli_cmd_info.call_args
+        sent_command = args[0]
+        expected_command = 'df -P'
+
+        self.assertEqual(sent_command, expected_command)
+
+    @patch.object(iiq_gather_info, 'cli_cmd_info')
+    def test_memory_info(self, fake_cli_cmd_info):
+        """iiq_gather_info.memory_info calls the expected command"""
+        iiq_gather_info.memory_info()
+        args, _ = fake_cli_cmd_info.call_args
+        sent_command = args[0]
+        expected_command = 'free -m'
+
+        self.assertEqual(sent_command, expected_command)
+
+    @patch.object(iiq_gather_info, 'cli_cmd_info')
+    def test_ifconfig_info(self, fake_cli_cmd_info):
+        """iiq_gather_info.ifconfig_info calls the expected command"""
+        iiq_gather_info.ifconfig_info()
+        args, _ = fake_cli_cmd_info.call_args
+        sent_command = args[0]
+        expected_command = 'ifconfig'
+
+        self.assertEqual(sent_command, expected_command)
+
+    @patch.object(iiq_gather_info, 'cli_cmd_info')
+    def test_iiq_version_info(self, fake_cli_cmd_info):
+        """iiq_gather_info.iiq_version_info calls the expected command"""
+        iiq_gather_info.iiq_version_info()
+        args, _ = fake_cli_cmd_info.call_args
+        sent_command = args[0]
+        expected_command = 'rpm -q isilon-insightiq'
+
+        self.assertEqual(sent_command, expected_command)
+
+class TestIiqGatheInfoMain(unittest.TestCase):
+    """A suite of tests for the iiq_gather_info.main function"""
+
+    @patch.object(iiq_gather_info.sys, 'stdout')
+    @patch.object(__builtin__, 'raw_input')
+    @patch.object(iiq_gather_info, 'get_tarfile')
+    @patch.object(iiq_gather_info, 'get_logger')
+    def test_main(self, fake_get_logger, fake_get_tarfile, fake_raw_input, fake_stdout):
+        """iiq_gather_info.main is callable, and returns an integer"""
+        fake_raw_input.return_value = 'yes'
+        exit_code = iiq_gather_info.main(['--case-number', '0', '--output-dir', '/tmp'])
+        expected = 0
+
+        self.assertEqual(exit_code, expected)
+
+    @patch.object(iiq_gather_info.sys, 'stdout')
+    @patch.object(__builtin__, 'raw_input')
+    @patch.object(iiq_gather_info, 'get_tarfile')
+    @patch.object(iiq_gather_info, 'get_logger')
+    def test_not_root_and_no(self, fake_get_logger, fake_get_tarfile, fake_raw_input, fake_stdout):
+        """If not ran as root, and you say `no` to the prompt, the script exit code is 1"""
+        fake_raw_input.return_value = 'no'
+        exit_code = iiq_gather_info.main(['--case-number', '0', '--output-dir', '/tmp'])
+        expected = 1
+
+        self.assertEqual(exit_code, expected)
+
+
+    @patch.object(iiq_gather_info.sys, 'stdout')
+    @patch.object(__builtin__, 'raw_input')
+    @patch.object(iiq_gather_info, 'get_tarfile')
+    @patch.object(iiq_gather_info, 'get_logger')
+    def test_ioerror(self, fake_get_logger, fake_get_tarfile, fake_raw_input, fake_stdout):
+        """An I/O (or OS) error when creating the tarfile returns a non-zero exit code"""
+        fake_raw_input.return_value = 'yes'
+        fake_get_tarfile.side_effect = [IOError(13, 'testing', 'some_file')]
+
+        exit_code = iiq_gather_info.main(['--case-number', '0', '--output-dir', '/tmp'])
+        expected = 13
+
+        self.assertEqual(exit_code, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
